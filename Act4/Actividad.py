aa4cf5cba30dc75877cc83a15928d7d94050580c
Biel <bielcasamor02@gmail.com>
2023-10-09 16:55:03 +0200

    Ej 2 dentro de act 4

---
+++ Act4/Actividad.py
@@ -0,0 +1,13 @@
+#Ejercicio 1
+print("Introduzca dos numeros y le dire que numeros ha introducido")
+A = int(input("Introduzca un numer A"))
+B = int(input ("Introduzca un numero B"))
+print("El numero A es:", A ,"El numero B es:",B)
+
+#Ejercicio 2
+
+print("Introduzca dos numeros y le dire el resultado de su suma, resta, multiplicacion y division")
+A = int(input("Introduzca un numer A"))
+B = int(input ("Introduzca un numero B"))
+print("El numero A es:", A ,"El numero B es:",B) 
+print ("suma = ",A+B," resta = ", A-B," multiplicacion = ",A*B," division (A/B) = ", A/B )
