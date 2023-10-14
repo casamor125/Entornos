14374a6c2d7ce132974eda2c935e0cc0f8b0253f
Biel <bielcasamor02@gmail.com>
2023-10-14 18:28:08 +0200

    actividad 5

---
+++ Act5/Actividad5.py
@@ -0,0 +1,21 @@
+#Ejercicio 1
+print("introduzca la base y la altura de un triangulo y le calcularemos el area")
+base = int(input("Introduzca la base del triangulo "))
+altura = int(input("Introduzca el altura del triangulo "))
+
+print("El area del triangulo es: ",(base*altura)/2)
+
+#Ejercicio 2
+
+print("introduzca dos numeros y hare la division del mayor entre el menor")
+
+A = int(input("Introduzca un numero A "))
+B= int(input("Introduzca un numero B "))
+
+if A>B:
+    print("A es el mayor y el resultado es: ", A/B)
+else:
+    print ("B es el mayor y el resultado es: ", B/A)    
+
+#Ejercicio3
+
