ec6507c4ca3c08f4286f3343851cd1ada14a0dea
Biel <bielcasamor02@gmail.com>
2023-10-16 16:56:23 +0200

    ejrecicio 4 de la entrega 5

--- Act5/Actividad5.py
+++ Act5/Actividad5.py
@@ -19,3 +19,18 @@ else:
 
 #Ejercicio3
 
+#Ejercicio4
+
+n = int (3)
+contraseña = str ('eureka')
+intento = str(input("Introduzca la contraseña: "))
+
+while(n>=2 and intento != "eureka"):
+    n = n-1
+    intento = input("Contraseña incorrecta, le quedan {n} intentos: ")
+    
+  
+if intento=="eureka":
+    print("Contraseña correcta")
+else:
+    print("Lo sentimos no te quedan intentos")
