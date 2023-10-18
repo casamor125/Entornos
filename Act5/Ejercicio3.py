4851a23a2359967246d20c69f101dc7ef669b654
Biel <bielcasamor02@gmail.com>
2023-10-18 19:42:46 +0200

    actividad tres de la 5 acabada

--- Act5/Ejercicio3.py
+++ Act5/Ejercicio3.py
@@ -1,7 +1,14 @@
 
 dia = int(input("Introduzca un dia "))
-mes = int(input("Introduzca un mes"))
+mes = int(input("Introduzca un mes "))
 
-if mes>0 and mes<12:
-    
-    
+if mes<=0 or mes>12:
+    print("la data es incorrecta")
+elif dia >31 or dia <=0:
+    print("la data es incorrecta")
+elif dia >30 and (mes == 2 or mes == 4 or mes == 6 or mes == 11 or mes == 9):
+          print("La data es incorrecta")
+elif dia > 28 and mes == 2:
+          print("La data es incorrecta")
+else:
+     print("la data ", dia ,"/",mes," es correcta")
