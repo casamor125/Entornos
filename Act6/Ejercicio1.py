d6f12de6e293863d666ca24453255dcd33f47adb
Biel <bielcasamor02@gmail.com>
2023-10-19 17:00:38 +0200

    ej 1 y 2

---
+++ Act6/Ejercicio1.py
@@ -0,0 +1,8 @@
+nom = input("introdueix el teu nom ")
+cogP = input("introdueix el teu cognom patern ")
+cogM = input("introdueix el teu cognom matern ")
+grau = input("introdueix el grau que curses ")
+color = input("introdueix el teu color preferit ")
+peli = input("introdueix la teva pelicula preferida ")
+
+print("L'alumne ",nom, cogP, cogM," cursa el grau",grau,", el color favorit és ",color," i la pel·lícula favorita és ",peli,".")
