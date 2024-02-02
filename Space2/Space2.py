4863526072ecd4b7a72317569589c3378378c2a8
Biel Casamor <bielcasamor02@gmail.com>
2024-02-02 19:42:56 +0100

    actualizacion pygame

--- Space2/Space2.py
+++ Space2/Space2.py
@@ -2,104 +2,70 @@ import pygame
 import Elementos2
 import random
 import pygame_menu
-#inicialicamos el juego
+
+# Inicializamos el juego
 pygame.init()
 
-#creamos la pantalla
+# Creamos la pantalla
 tamaño = (1000, 800)
 pantalla = pygame.display.set_mode(tamaño)
 
-#creamos un reloj
+# Creamos un reloj
 reloj = pygame.time.Clock()
-FPS = 18
+FPS = 30
 
-# creamos una fuente para la pausa
+# Creamos una fuente para la pausa
 font = pygame.font.Font(None, 30)
 
-#booleano de control
-# posicion = (650,700)
-# nave = Elementos2.Nave(posicion)
-# fondo = Elementos2.Fondo()
-# #crear un grupo de sprites
-# #grupo_sprites = pygame.sprite.Group(fondo)
-# #grupo_sprites.add(nave)
-#
-# #grupo_sprites.add(Elementos2.Nave((400,200)))
-# #grupo_sprites.add(Elementos2.Nave((500,100)))
-# #grupo_sprites.add(Elementos2.Nave((100,300)))
-#
-# grupo_sprites_todos = pygame.sprite.Group()
-# grupo_sprites_enemigos = pygame.sprite.Group()
-# grupo_sprites_bala = pygame.sprite.Group()
-#
-# # grupo_sprites_todos.add(Elementos2.Fondo, (0, ))
-# grupo_sprites_todos.add(fondo)
-# grupo_sprites_todos.add(nave)
-# enemigo = Elementos2.Enemigo((50,50))
-# grupo_sprites.add(Elementos2.Enemigo((70, 70)))
-
-#crear u na variable que almacene la última creacion de enemigo
+# Variables globales
 ultimo_enemigo_creado = 0
-frecuencia_creacion_enemigo = 750
+ultimo_astronauta_creado = 0
+frecuencia_creacion_enemigo = 250  # Valor por defecto
+frecuencia_creacion_astronauta = 750  # Valor por defecto
 
 def set_difficulty(value, difficulty):
-    # Do the job here !
     global frecuencia_creacion_enemigo
     frecuencia_creacion_enemigo = difficulty
-    pass
+    global frecuencia_creacion_astronauta
+    frecuencia_creacion_astronauta = difficulty
 
 def start_the_game():
-    # Do the job here !
     running = [True]
     global ultimo_enemigo_creado
     global ultimo_astronauta_creado
-    global frecuencia_creacion_enemigo
-    global FPS
-    global reloj
+
     posicion = (650, 700)
     nave = Elementos2.Nave(posicion)
     fondo = Elementos2.Fondo()
-    # crear un grupo de sprites
-    # grupo_sprites = pygame.sprite.Group(fondo)
-    # grupo_sprites.add(nave)
-
-    # grupo_sprites.add(Elementos2.Nave((400,200)))
-    # grupo_sprites.add(Elementos2.Nave((500,100)))
-    # grupo_sprites.add(Elementos2.Nave((100,300)))
 
     grupo_sprites_todos = pygame.sprite.Group()
     grupo_sprites_enemigos = pygame.sprite.Group()
+    grupo_sprites_astronautas = pygame.sprite.Group()
     grupo_sprites_bala = pygame.sprite.Group()
 
-
-     # grupo_sprites_todos.add(Elementos2.Fondo, (0, - ))
     grupo_sprites_todos.add(fondo)
     grupo_sprites_todos.add(nave)
 
     pausado = False
 
-    # capturamos las teclas
-
-    # bucle principal
     while running[0]:
-        # Limitamos el bucle al framrate definido
         reloj.tick(FPS)
-        # gestionar la salida
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = [False]
 
         teclas = pygame.key.get_pressed()
+
         if teclas[pygame.K_ESCAPE]:
             running[0] = False
 
         if teclas[pygame.K_SPACE]:
             pausado = not pausado
 
-
         if not pausado:
-            # creacion de enemigos
             momento_actual = pygame.time.get_ticks()
+
             if (momento_actual > ultimo_enemigo_creado + frecuencia_creacion_enemigo):
                 cordX = random.randint(0, pantalla.get_width())
                 cordY = 0
@@ -108,47 +74,34 @@ def start_the_game():
                 grupo_sprites_enemigos.add(enemigo)
                 ultimo_enemigo_creado = momento_actual
 
-        # Asegúrate de tener estas líneas al principio del bucle principal
-        ultimo_astronauta_creado = 0
-        frecuencia_creacion_astronauta = 1000  # Puedes ajustar la frecuencia según sea necesario
-        grupo_sprites_astronautas = pygame.sprite.Group()      
-        if (momento_actual > ultimo_astronauta_creado + frecuencia_creacion_astronauta):
-            cordX = random.randint(0, pantalla.get_width())
-            cordY = 0
-            astronauta = Elementos2.Astronauta((cordX, cordY))
-            grupo_sprites_todos.add(astronauta)
-            grupo_sprites_astronautas.add(astronauta)
-            ultimo_astronauta_creado = momento_actual
-
-# ...
-
-
+            if (momento_actual > ultimo_astronauta_creado + frecuencia_creacion_astronauta):
+                cordX = random.randint(0, pantalla.get_width())
+                cordY = 0
+                astronauta = Elementos2.Astronauta((cordX, cordY))
+                grupo_sprites_todos.add(astronauta)
+                grupo_sprites_astronautas.add(astronauta)
+                ultimo_astronauta_creado = momento_actual
 
             grupo_sprites_todos.update(teclas, grupo_sprites_todos, grupo_sprites_bala, grupo_sprites_enemigos,
-                                           running)
-            
-        # capturamos las teclas
-
-        # pintaremos:
-        # pantalla.fill((255,255,255))
-        # grupo_sprites_todos.update(teclas, grupo_sprites_todos, grupo_sprites_bala, grupo_sprites_enemigos, running)
-        grupo_sprites_todos.draw(pantalla)
+                                       running)
+            grupo_sprites_todos.update(teclas, grupo_sprites_todos, grupo_sprites_bala, grupo_sprites_enemigos, running)
+            grupo_sprites_todos.draw(pantalla)
 
         if pausado:
             texto = font.render("PAUSA", True, "White")
             pantalla.blit(texto, (pantalla.get_width() / 2, pantalla.get_height() / 2))
 
-        # redibujar la pantala
         pygame.display.flip()
-    pass
 
+# Menú principal
 menu = pygame_menu.Menu('Welcome', 400, 300, theme=pygame_menu.themes.THEME_BLUE)
-
 menu.add.text_input('Name :', default='')
 menu.add.selector('Difficulty :', [('Hard', 200), ('Easy', 2000)], onchange=set_difficulty)
 menu.add.button('Play', start_the_game)
 menu.add.button('Quit', pygame_menu.events.EXIT)
 
+# Mostramos el menú y ejecutamos el juego
 menu.mainloop(pantalla)
-#finalizamos el juego
+
+# Finalizamos el juego
 pygame.quit()
